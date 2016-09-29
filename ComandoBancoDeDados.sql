69535af2af4d91c0f8b958bcdd28c2cca200d1ce
CBassi <cayogeral@hotmail.com>
2016-09-28 21:36:49 -0300

    Update ComandoBancoDeDados.sql

--- ComandoBancoDeDados.sql
+++ ComandoBancoDeDados.sql
@@ -2,12 +2,12 @@ create database tabela10k;
 
 use tabela10k;
 
-create table trajeto (id BIGINT NOT NULL AUTO_INCREMENT, IDARQUIVO bigint(100) not null,data varchar(100) not null, hora varchar(100) ,LATITUDE float(20) not null,LONGITUDE float(20) not null, PRIMARY KEY (ID));
+create table trajetoComIndice (id BIGINT NOT NULL AUTO_INCREMENT, IDARQUIVO bigint(100) not null,data varchar(100) not null, hora varchar(100) ,LATITUDE float(20) not null,LONGITUDE float(20) not null, PRIMARY KEY (ID));
 
-create table trajetoIndex (id BIGINT NOT NULL AUTO_INCREMENT, IDARQUIVO bigint(100) not null,data varchar(100) not null, hora varchar(100) ,LATITUDE float(20) not null,LONGITUDE float(20) not null, PRIMARY KEY (ID));
+create table trajetoSemIndice (id BIGINT NOT NULL AUTO_INCREMENT, IDARQUIVO bigint(100) not null,data varchar(100) not null, hora varchar(100) ,LATITUDE float(20) not null,LONGITUDE float(20) not null, PRIMARY KEY (ID));
 
 -->CRIANDO INDEX
-	ALTER TABLE trajetoIndex ADD INDEX(IDARQUIVO);
+	ALTER TABLE trajetoComIndex ADD INDEX(IDARQUIVO);
 
 
 
